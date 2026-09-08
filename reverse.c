bc9ed253e857744450999b0a210006d251b0c436
dishaborude7-tech <dishaborude7@gmail.com>
2026-09-08 22:27:09 +0530

    reverse

--- reverse.c
+++ reverse.c
@@ -60,29 +60,71 @@ void create_node()
     }
 }
 
-int mid_point_address()
+struct node *mid_point_address()
 {
-    struct node *slow,*fast;
+    struct node *odd, *even;
+
     if(START==NULL)
     {
-        printf("the list is empty\n");
+        printf("The list is empty\n");
+        return NULL;
     }
     else
     {
-        slow=fast=START;
-        while(fast!=NULL && fast->next!=NULL)
+        odd=START;
+        even=START;
+
+        while(even!=NULL && even->next!=NULL)
         {
-            slow=slow->next;
-            fast=fast->next->next;
+            odd=odd->next;
+            even=even->next->next;
         }
     }
-    return slow;
+
+    return odd;
 }
+
 // Function to reverse the singly linked list 
 void reverse_nodes()
  {
-    int temp,length=0,i;
+    int temp,len=1,i;
     struct node *first,*last,*mid;
-    
-    
+    last=START;
+    while(last->next!=NULL)//stop at the last node of the linked list
+    {
+        len++;
+        last=last->next;
+    }
+    first=START;//stop at the first node of the linked list
+    mid=mid_point_address();//stop at the middle node of the linked list
+    for(i=0;i<len/2;i++)
+    {
+        while(mid->next!=last)//stop at the middle node of the linked list
+        {
+            mid=mid->next;//move mid pointer to its position
+        }
+        temp=first->data;
+        first->data=last->data;
+        last->data=temp;
+        first=first->next;//move first pointer to the next node
+        last=mid;//move last pointer to the middle node
+        mid=mid_point_address();//move mid pointer to the middle node of the linked list
+    }
  }
+
+int main()
+{
+    int size;
+    printf("enter the size of linked list:");
+    scanf("%d",&size);
+    while(size>0)
+    {
+        create_node();
+        size--;
+    }
+    printf("Original linked list:\n");
+    display();
+    reverse_nodes();
+    printf("\nReversed linked list:\n");
+    display();
+}
